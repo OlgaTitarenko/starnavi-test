4412dc5e5f216c84a9bc05330e9c4e4ac00d1f77
титаренко <kizuneworld@gmail.com>
2019-06-20 19:12:53 +0300

    Updates

---
+++ precache-manifest.018f5f4a341321ff2e0ac89848b21d34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44dc537eb7d6d5f961e072083bb2354",
+    "url": "/starnavi-test/index.html"
+  },
+  {
+    "revision": "40f87915f0a91c1533d2",
+    "url": "/starnavi-test/static/css/main.8d968bab.chunk.css"
+  },
+  {
+    "revision": "1ff20e332461771659ed",
+    "url": "/starnavi-test/static/js/2.fb726950.chunk.js"
+  },
+  {
+    "revision": "40f87915f0a91c1533d2",
+    "url": "/starnavi-test/static/js/main.7a8c3104.chunk.js"
+  },
+  {
+    "revision": "102bfd312c2172e83bbf",
+    "url": "/starnavi-test/static/js/runtime~main.18184c18.js"
+  }
+]);
